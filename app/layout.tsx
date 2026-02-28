aa7ccd0841d4f7efdc1f03da725d1aae7971cabe
Abuchtela <84213452+Abuchtela@users.noreply.github.com>
2026-02-28 18:53:27 -0500

    Fix TalentApp verification by adding it to head tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,13 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: `${NAME} — Get Paid to Build`,
     description: 'Track your Neynar score, Builder Score, and earn ETH for shipping on Base.',
+    verification: {
+      other: {
+        'talentapp:project_verification': 'f86b68df629f6ac7af584584b2a3f5110982b8231aa9859e937a4b47e11830bbf3686f6d1a68aebdb9db4f86e2ca9e5871a812f287b45c1a32ca32678c790700',
+        'coinbase-verification': 'bc_hi2cipof',
+      }
+    },
     other: {
-      'talentapp:project_verification': 'f86b68df629f6ac7af584584b2a3f5110982b8231aa9859e937a4b47e11830bbf3686f6d1a68aebdb9db4f86e2ca9e5871a812f287b45c1a32ca32678c790700',
-      'coinbase-verification': 'bc_hi2cipof',
       'fc:frame': JSON.stringify({
         version: 'next',
         imageUrl: `${URL}/hero.png`,
@@ -33,6 +37,10 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="talentapp:project_verification" content="f86b68df629f6ac7af584584b2a3f5110982b8231aa9859e937a4b47e11830bbf3686f6d1a68aebdb9db4f86e2ca9e5871a812f287b45c1a32ca32678c790700" />
+        <meta name="coinbase-verification" content="bc_hi2cipof" />
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
